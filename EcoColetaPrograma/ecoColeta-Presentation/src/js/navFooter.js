c1c7623d2b6caf85475189b277099f58cc49f2a8
kauangomesm <kayangomes849@gmail.com>
2025-05-15 21:47:17 -0300

    footer atualizado

--- EcoColetaPrograma/ecoColeta-Presentation/src/js/navFooter.js
+++ EcoColetaPrograma/ecoColeta-Presentation/src/js/navFooter.js
@@ -415,6 +415,137 @@ document.addEventListener('DOMContentLoaded', function() {
   } else {
     console.error('Alguns elementos do menu não foram encontrados!');
   }
+
+
+
+  // Criando Footer
+
+let footer = document.createElement('footer');
+footer.id = 'footer';
+
+let divfooter = document.createElement('div');
+divfooter.className = 'footer';
+
+let footerContent = document.createElement('div');
+footerContent.className = 'footer-content';
+
+let footerLogo = document.createElement('div');
+footerLogo.className = 'footer-logo';
+
+// Wrapper que agrupa imagem e título
+let footerLogoWrapper = document.createElement('div');
+footerLogoWrapper.className = 'footer-logo-wrapper';
+
+let footerLogoImg = document.createElement('img');
+footerLogoImg.src = 'https://cdn.builder.io/api/v1/image/assets/TEMP/313a86a3763eb086a92f424be391c74cac3baa49?placeholderIfAbsent=true&apiKey=9d5f0dcfa7364879a1f7ce16420e4a1e';
+footerLogoImg.alt = 'EcoColeta';
+
+let footerLogoH3 = document.createElement('h3');
+footerLogoH3.textContent = 'EcoColeta';
+
+footerLogoWrapper.appendChild(footerLogoImg);
+footerLogoWrapper.appendChild(footerLogoH3);
+
+let footerLogoP = document.createElement('p');
+footerLogoP.textContent = 'Transformando a reciclagem em uma experiência recompensadora.';
+
+// Adiciona wrapper e parágrafo no logo
+footerLogo.appendChild(footerLogoWrapper);
+footerLogo.appendChild(footerLogoP);
+
+// Criando grupos de links
+let footerLinks = document.createElement('div');
+footerLinks.className = 'footer-links';
+
+function createLinkGroup(title, links) {
+  let group = document.createElement('div');
+  group.className = 'link-group';
+
+  let h4 = document.createElement('h4');
+  h4.textContent = title;
+
+  let ul = document.createElement('ul');
+  links.forEach(link => {
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    a.href = link.href;
+    a.textContent = link.text;
+    li.appendChild(a);
+    ul.appendChild(li);
+  });
+
+  group.appendChild(h4);
+  group.appendChild(ul);
+  return group;
+}
+
+let group1 = createLinkGroup('Links Rápidos', [
+  { text: 'Sobre Nós', href: '#' },
+  { text: 'Como Funciona', href: '#' },
+  { text: 'Pontos de Coleta', href: '#' },
+  { text: 'Recompensas', href: '#l' }
+]);
+
+let group2 = createLinkGroup('Suporte', [
+  { text: 'FAQ', href: '#' },
+  { text: 'Contato', href: '#' },
+  { text: 'Política de Privacidade', href: '#' },
+  { text: 'Termos de Uso', href: '#' }
+]);
+
+
+let group3 = document.createElement('div');
+group3.className = 'link-group';
+
+let group3H4 = document.createElement('h4');
+group3H4.textContent = 'Redes Sociais';
+
+let sociaisIcons = document.createElement('div');
+sociaisIcons.className = 'social-icons';
+
+let redes = ['facebook', 'twitter', 'instagram', 'linkedin'];
+let redesLinks = [
+  'https://www.facebook.com/',
+  'https://twitter.com/',
+  'https://www.instagram.com/',
+  'https://www.linkedin.com/'
+];
+redes.forEach((rede, index) => {
+  let a = document.createElement('a');
+  a.href = redesLinks[index];
+  a.target = '_blank';
+  a.rel = 'noopener noreferrer';
+
+let imgsoial = document.createElement('img');
+imgsoial.src = `assets/img/${rede}.svg`;
+a.appendChild(imgsoial);
+sociaisIcons.appendChild(a);
+});
+
+group3.appendChild(group3H4);
+group3.appendChild(sociaisIcons);
+
+// Agrupando os grupos
+footerLinks.appendChild(group1);
+footerLinks.appendChild(group2);
+footerLinks.appendChild(group3);
+
+footerContent.appendChild(footerLogo);
+footerContent.appendChild(footerLinks);
+
+// Copyright
+let copyright = document.createElement('div');
+copyright.className = 'copyright';
+
+let pCopyright = document.createElement('p');
+pCopyright.innerHTML = '&copy; 2023 EcoColeta. Todos os direitos reservados.';
+
+copyright.appendChild(pCopyright);
+divfooter.appendChild(footerContent);
+divfooter.appendChild(copyright);
+footer.appendChild(divfooter);
+
+tagScript.parentNode.insertBefore(footer, tagScript);
 });
 
 // Inicializar interface
