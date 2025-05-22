3f51c76d1b48c2b863e21e63ef9bc029af3b7649
Ronaldo Augusto Oliveira Lacerda <ro.augustolacerda@gmail.com>
2025-05-21 22:00:33 -0300

    recompensas integrado

--- EcoColetaPrograma/ecoColeta-Presentation/src/js/recompensas.js
+++ EcoColetaPrograma/ecoColeta-Presentation/src/js/recompensas.js
@@ -1,6 +1,6 @@
-document.querySelectorAll('.card-btn').forEach(btn => {
+document.querySelectorAll('.recompensas-card-btn').forEach(btn => {
   btn.addEventListener('click', function() {
-    if (btn.classList.contains('doar')) {
+    if (btn.classList.contains('recompensas-doar')) {
       alert('Obrigado por doar seus pontos para projetos ambientais!');
     } else {
       alert('Recompensa resgatada com sucesso!');
